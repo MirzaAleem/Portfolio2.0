45fbec51a1f0d3919960d27ad94fdc0959fb137d
Mirza Aleem Abbas <aleemabbas31@gmail.com>
2024-02-02 23:51:12 +0530

    Added a new project in project section.

--- src/data.js
+++ src/data.js
@@ -1,5 +1,12 @@
 export const Info = [
     {
+        
+        title : "Chat App",
+        description : "This is a chat app made using reactjs and firebase for backend.",
+        image : "assets/images/chat-app.png",
+        url : "https://aleem-chat-app.vercel.app/"
+    },
+    {
         title : "Movie Search Engine",
         description : "Using OMDb API for Movie related info, created this responsive webpage in reactjs.",
         image : "assets/images/project1.png",
@@ -8,7 +15,7 @@ export const Info = [
     
     {
         title : "Social Media Website",
-        description : "Currently it is only the Front-end, I'm working on the Backend !!",
+        description : "This is a social media website made using MERN stack.",
         image : "assets/images/Ayila.png",
         url : "https://ayila.netlify.app/"
     },
